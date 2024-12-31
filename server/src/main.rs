bf13841a1433f15e0c8ce73b4e6d79c300783d67
yrf_tan <3262975463@qq.com>
2024-12-31 22:35:27 +0800

    feat:测试sqlite数据库的使用

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use rusqlite::{Connection, Result};
+use std::env;
+use std::path::PathBuf;
+
+#[tokio::main]
+async fn main() {
+    match env::current_dir() {
+        Ok(cur_dir) => {
+            let db_path = cur_dir.join("tancloud.db");
+            let conn = Connection::open(db_path);
+            // 在这里处理数据库连接
+        }
+        Err(e) => eprintln!("无法获取当前的工作目录路径: {}", e),
+    }
+
+    // 初始化 tracing 用于记录日志
+    tracing_subscriber::fmt::init();
+
+    // 设置路由
+    let app = Router::new()
+        .route("/TanCloud", get(root))
+        .route("/TanCloud", post(root));
+
+    // 使用 hyper
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:52996")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> & 'static str {
+    "你好个p"
 }
