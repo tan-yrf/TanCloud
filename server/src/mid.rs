81abff9ce8c0dcfc904d4cf78d92519e9a86a6f7
yrf_tan <3262975463@qq.com>
2025-01-02 00:43:27 +0800

    feat：中间件，用来过滤非本机ip的请求

---
+++ server/src/mid.rs
@@ -0,0 +1,16 @@
+use axum::{
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::{IntoResponse, Response},
+    body::Body,
+};
+use std::net::SocketAddr;
+
+pub async fn restrict_to_localhost(req: Request<Body>, next: Next) -> Response {
+    if let Some(addr) = req.extensions().get::<SocketAddr>() {
+        if addr.ip().is_loopback() {
+            return next.run(req).await;
+        }
+    }
+    (StatusCode::FORBIDDEN, "Forbidden").into_response()
+}
