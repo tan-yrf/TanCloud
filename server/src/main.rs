d78e09d08d289d4eee96f8ca07100089beab9cd0
yrf_tan <3262975463@qq.com>
2025-01-02 00:44:34 +0800

    test：测试创建用户

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,44 +1,43 @@
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
     routing::{get, post},
-    Json, Router,
+    response::IntoResponse,
+    Json,
+    Router,
+    middleware,
 };
-use serde::{Deserialize, Serialize};
-use sqlx::sqlite::SqlitePool;
-use once_cell::sync::OnceCell;
-use std::env;
-use std::path::PathBuf;
+
+use tracing_subscriber;
+use serde_json::json;
+
+mod db;
+mod error_code;
+mod http_response;
+mod mid;
+
 
 #[tokio::main]
 async fn main() {
-    // match env::current_dir() {
-    //     Ok(cur_dir) => {
-    //         let db_path = cur_dir.join("tancloud.db");
-    //         let conn = Connection::open(db_path);
-    //         // 在这里处理数据库连接
-    //     }
-    //     Err(e) => eprintln!("无法获取当前的工作目录路径: {}", e),
-    // }
+    // 初始化数据库
+    db::config::init_db().await;
 
-    
-    // 初始化 tracing 用于记录日志
+    // 初始化 tracing 用来记录日志
     tracing_subscriber::fmt::init();
 
-    // 设置路由
-    let app = Router::new()
-        .route("/TanCloud", get(root))
-        .route("/TanCloud", post(root));
+    // 构建路由
+    //let user_routes = Router::new();
 
-    // 使用 hyper
+    let app = Router::new()
+        .route("/TanCloud", post(root))
+        .nest("/TanCloud/user", user_routes);
+
+    // 使用 hyper 运行服务
     let listener = tokio::net::TcpListener::bind("0.0.0.0:52996")
         .await
         .unwrap();
-
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> & 'static str {
-    "你好个p"
+async fn root() -> impl IntoResponse {
+    Json(json!({ "message": "你好个p" }))
 }
