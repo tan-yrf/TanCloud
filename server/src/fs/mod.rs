837b4d5d07bbba22d678c984e88beb64e42b98dd
yrf_tan <3262975463@qq.com>
2025-01-05 22:22:44 +0800

    feat：减少不必要的路由

--- server/src/fs/mod.rs
+++ server/src/fs/mod.rs
@@ -1,4 +1,6 @@
 pub mod info;
+pub mod rename;
+pub mod tools;
 
 use axum::{
     Router, 
@@ -8,10 +10,6 @@ use axum::{
 use crate::mid::RequireAuth;
 
 pub fn fs_routes() -> Router {
-    Router::new()
-        .nest("/", info_routes())
-}
-pub fn info_routes() -> Router {
     Router::new()
         .route("/get_directory_info", post(info::handle_get_directory_info)) 
         .route_layer(from_extractor::<RequireAuth>())
