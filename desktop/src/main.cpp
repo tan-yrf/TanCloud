6d507e9eb589457490c485edbea361068fa037ca
tan <3262975463@qq.com>
2025-01-12 22:38:34 +0800

    feat：调整引用模块

--- desktop/src/main.cpp
+++ desktop/src/main.cpp
@@ -1,8 +1,12 @@
+#include "Login/Login.h"
+
 #include <QApplication>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+    Login login;
+    login.show();
 
     return a.exec();
 }
