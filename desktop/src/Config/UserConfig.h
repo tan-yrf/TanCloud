c3b41fcb4002cb20c2ce9ece8a83e8293b398a82
tan <3262975463@qq.com>
2025-01-15 15:14:00 +0800

    feat：增加用户配置

---
+++ desktop/src/Config/UserConfig.h
@@ -0,0 +1,11 @@
+#ifndef USERCONFIG_H
+#define USERCONFIG_H
+
+#include <QString>
+
+namespace UserConfig {
+    static QString id;
+    static QString name;
+    static qint64 space;
+}
+#endif // USERCONFIG_H
